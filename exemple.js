a7910229c1191c9a003667bb909fcc2480a03c7f
Malal91 <controlmalal@gmail.com>
2018-07-05 22:25:46 +0000

    readme

--- exemple.js
+++
@@ -1,4 +0,0 @@
-const {getPrefectures, getQuartiers, getSousprefectures, getRegions, getCommunes, getSousprefecturesByPrefecture, getQuartiersByCommune} = require('./')
-
-var r = getQuartiersByCommune('matoto')
-console.log(r)
